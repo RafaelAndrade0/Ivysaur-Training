382c4f1c67e2f1d3c23850e91e2d5fe303708ac6
Rafael Andrade <rafael.andrade.dev0@gmail.com>
2020-04-02 12:21:58 -0300

    Finalizando home component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,8 +4,10 @@ import { CommonModule } from "@angular/common";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./page/home.component";
 
+import { ButtonsModule } from "ngx-bootstrap/buttons";
+
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule]
+  imports: [CommonModule, HomeRoutingModule, ButtonsModule]
 })
 export class HomeModule {}
--- src/app/modules/home/page/home.component.ts
+++ src/app/modules/home/page/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  constructor(
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goToBasicTraining() {
+    this.router.navigate(["/basic-training"], { relativeTo: this.route });
   }
 
+  goToIntermediateTraining() {
+    this.router.navigate(["/intermediate-training"], {
+      relativeTo: this.route
+    });
+  }
 }
